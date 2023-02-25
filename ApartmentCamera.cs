b3f1a86d9bb1d6d8a46090a0e2a451c02b59f673
Adrian Burgos <awb8593@rit.edu>
2023-02-25 15:02:41 -0500

    Updated office camera script

--- ApartmentCamera.cs
+++ ApartmentCamera.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apartment : MonoBehaviour
+public class ApartmentCamera : MonoBehaviour
 {
+    public float maxX;
+    public float minX;
+    public float moveSpeed;
+
     void Update()
     {
+        if (Input.GetAxis("Mouse X") != 0) {
+            transform.position += new Vector3 (Input.GetAxisRaw("Mouse X") * Time.deltaTime * moveSpeed, 0.0f, 0.0f);
+        }
+
+        transform.position = new Vector3 (Mathf.Clamp(transform.position.x, minX, maxX), transform.position.y, transform.position.z);
+        /**
         float rotateHorizontal = Input.GetAxis ("Mouse X");
-        if (rotateHorizontal > 120) {
-            rotateHorizontal = 120;
+        if (rotateHorizontal > maxX) {
+            rotateHorizontal = maxX;
         }
-        if (rotateHorizontal < -120) {
-            rotateHorizontal = -120
+        if (rotateHorizontal < minX) {
+            rotateHorizontal = minX;
         }
+        */
     }
 }
